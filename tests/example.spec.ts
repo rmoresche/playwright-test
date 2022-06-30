d3f430ef8dd485ea9f8afbd0564911d906e00ad4
rmoresche <renato.mello@brisken.com>
2022-06-30 16:42:49 -0300

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http:localhost:3000/');
+});
+
+test.describe('Add fruit', () => {
+  test('Should contain 3 elements on the list and a fruit called test', async ({ page }) => {
+    const fruitAdded = {name: 'test'}
+    
+    await page.locator('input').click();
+    
+    await page.locator('input').fill('test');
+    
+    await page.locator('text=Add fruit').click();
+
+    await expect(page.locator('li')).toHaveCount(3);
+
+    await expect(page.locator('li:nth-child(3)')).toContainText(fruitAdded.name)
+  });
+});
